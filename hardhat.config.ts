b5a56719fab1b7829ab8f33c57f79e325b44bfa9
detoo <daily.detoo@gmail.com>
2022-04-26 23:41:42 -0700

    setup repo

---
+++ hardhat.config.ts
@@ -0,0 +1,46 @@
+import "@nomiclabs/hardhat-ethers"
+import "@nomiclabs/hardhat-waffle"
+import "@openzeppelin/hardhat-upgrades"
+import "@typechain/hardhat"
+import "hardhat-contract-sizer"
+import "hardhat-gas-reporter"
+import { HardhatUserConfig } from "hardhat/config"
+import "solidity-coverage"
+import "./mocha-test"
+
+const config: HardhatUserConfig = {
+    solidity: {
+        version: "0.7.6",
+        settings: {
+            optimizer: { enabled: true, runs: 100 },
+            evmVersion: "berlin",
+            // for smock to mock contracts
+            outputSelection: {
+                "*": {
+                    "*": ["storageLayout"],
+                },
+            },
+        },
+    },
+    networks: {
+        hardhat: {
+            allowUnlimitedContractSize: true,
+        },
+    },
+    contractSizer: {
+        alphaSort: true,
+        runOnCompile: true,
+        disambiguatePaths: false,
+    },
+    gasReporter: {
+        excludeContracts: ["test"],
+    },
+    mocha: {
+        require: ["ts-node/register/files"],
+        jobs: 4,
+        timeout: 120000,
+        color: true,
+    },
+}
+
+export default config
