7dfa35891232b357290ef46d2dea5c66dbb935cf
42bchen <boyuchen0224@gmail.com>
2022-07-01 11:31:03 +0800

    Add scripts

---
+++ scripts/path.ts
@@ -0,0 +1,9 @@
+export interface ContractNameAndDir {
+    name: string
+    dir: string
+}
+
+// only for slither analysis.
+export const allDeployedContractsNamesAndDirs: ContractNameAndDir[] = [
+    // { name: "SurplusBeneficiary.sol", dir: "./contracts" },
+]
---
+++ scripts/prepack.ts
@@ -0,0 +1,16 @@
+import { asyncExec } from "./helper"
+
+async function main(): Promise<void> {
+    await asyncExec("rm -rf artifacts/contracts/test/")
+    await asyncExec("find artifacts/contracts/ -name '*.dbg.json' -delete")
+    await asyncExec("rm -rf contracts/test")
+}
+
+if (require.main === module) {
+    main()
+        .then(() => process.exit(0))
+        .catch(error => {
+            console.error(error)
+            process.exit(1)
+        })
+}
