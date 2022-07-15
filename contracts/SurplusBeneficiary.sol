ecce251b19034c27707b81bc98b3f3afcdb2d279
42bchen <boyuchen0224@gmail.com>
2022-07-15 14:54:44 +0800

    Immutable

--- contracts/SurplusBeneficiary.sol
+++ contracts/SurplusBeneficiary.sol
@@ -2,78 +2,70 @@
 pragma solidity 0.7.6;
 pragma abicoder v2;
 
-import { AddressUpgradeable } from "@openzeppelin/contracts-upgradeable/utils/AddressUpgradeable.sol";
-import { SafeMathUpgradeable } from "@openzeppelin/contracts-upgradeable/math/SafeMathUpgradeable.sol";
-import { ReentrancyGuardUpgradeable } from "@openzeppelin/contracts-upgradeable/utils/ReentrancyGuardUpgradeable.sol";
-import {
-    SafeERC20Upgradeable,
-    IERC20Upgradeable
-} from "@openzeppelin/contracts-upgradeable/token/ERC20/SafeERC20Upgradeable.sol";
-import { OwnableUpgradeable } from "@openzeppelin/contracts-upgradeable/access/OwnableUpgradeable.sol";
-import { SafeMathUpgradeable } from "@openzeppelin/contracts-upgradeable/math/SafeMathUpgradeable.sol";
+import { Address } from "@openzeppelin/contracts/utils/Address.sol";
+import { SafeMath } from "@openzeppelin/contracts/math/SafeMath.sol";
+import { ReentrancyGuard } from "@openzeppelin/contracts/utils/ReentrancyGuard.sol";
+import { SafeERC20, IERC20 } from "@openzeppelin/contracts/token/ERC20/SafeERC20.sol";
+import { Ownable } from "@openzeppelin/contracts/access/Ownable.sol";
+import { SafeMath } from "@openzeppelin/contracts/math/SafeMath.sol";
 import { FullMath } from "@uniswap/v3-core/contracts/libraries/FullMath.sol";
 import { SurplusBeneficiaryStorageV1 } from "./storage/SurplusBeneficiaryStorage.sol";
 import { ISurplusBeneficiary } from "./interface/ISurplusBeneficiary.sol";
 import { IFeeDistributor } from "./interface/IFeeDistributor.sol";
 
-contract SurplusBeneficiary is
-    ISurplusBeneficiary,
-    ReentrancyGuardUpgradeable,
-    OwnableUpgradeable,
-    SurplusBeneficiaryStorageV1
-{
-    using AddressUpgradeable for address;
-    using SafeMathUpgradeable for uint256;
+contract SurplusBeneficiary is ISurplusBeneficiary, ReentrancyGuard, Ownable, SurplusBeneficiaryStorageV1 {
+    using Address for address;
+    using SafeMath for uint256;
 
-    /// @dev this function is public for testing
-    // solhint-disable-next-line func-order
-    function initialize(
+    constructor(
         address tokenArg,
         address feeDistributorArg,
         address treasuryArg,
         uint24 treasuryPercentageArg
-    ) public initializer {
-        __ReentrancyGuard_init();
-        __Ownable_init();
+    ) ReentrancyGuard() Ownable() {
+        // SB_TANC: token is not a contract
+        require(tokenArg.isContract(), "SB_TANC");
+        _token = tokenArg;
 
-        setToken(tokenArg);
         setFeeDistributor(feeDistributorArg);
         setTreasury(treasuryArg);
         setTreasuryPercentage(treasuryPercentageArg);
 
-        IERC20Upgradeable(_token).approve(feeDistributorArg, uint256(-1));
+        IERC20(_token).approve(feeDistributorArg, uint256(-1));
     }
 
     //
     // PUBLIC ONLY-ADMIN NON-VIEW
     //
 
-    function setToken(address tokenArg) public onlyOwner {
-        // SB_TANC: token address is not contract
-        require(tokenArg.isContract(), "SB_TANC");
-        emit TokenChanged(_token, tokenArg);
-        _token = tokenArg;
-    }
-
     function setFeeDistributor(address feeDistributorArg) public onlyOwner {
         // SB_FDNC: feeDistributor address is not contract
         require(feeDistributorArg.isContract(), "SB_FDNC");
-        emit FeeDistributorChanged(_feeDistributor, feeDistributorArg);
+
+        // SB_TNM: token is not match
+        require(IFeeDistributor(feeDistributorArg).token() == _token, "SB_TNM");
+
+        address oldFeeDistributor = _feeDistributor;
         _feeDistributor = feeDistributorArg;
+        emit FeeDistributorChanged(oldFeeDistributor, feeDistributorArg);
     }
 
     function setTreasury(address treasuryArg) public onlyOwner {
-        // SB_TNC: treasury address is not contract
-        require(treasuryArg.isContract(), "SB_TNC");
-        emit TreasuryChanged(_treasury, treasuryArg);
+        // SB_TZ: treasury address is zero
+        require(treasuryArg != address(0), "SB_TZ");
+
+        address oldTreasury = _treasury;
         _treasury = treasuryArg;
+        emit TreasuryChanged(oldTreasury, treasuryArg);
     }
 
     function setTreasuryPercentage(uint24 treasuryPercentageArg) public onlyOwner {
-        // SB_TPZ: treasury percentage is equal to zero
-        require(treasuryPercentageArg != 0, "SB_TPZ");
-        emit TreasuryPercentageChanged(_treasuryPercentage, treasuryPercentageArg);
+        // SB_TPO: treasury percentage out of bound
+        require(treasuryPercentageArg <= 1e6, "SB_TPO");
+
+        uint24 oldTreasuryPercentage = _treasuryPercentage;
         _treasuryPercentage = treasuryPercentageArg;
+        emit TreasuryPercentageChanged(oldTreasuryPercentage, treasuryPercentageArg);
     }
 
     //
@@ -81,10 +73,10 @@ contract SurplusBeneficiary is
     //
 
     /// @inheritdoc ISurplusBeneficiary
-    function dispatch() external override nonReentrant returns (uint256) {
+    function dispatch() external override nonReentrant {
         address token = _token;
 
-        uint256 tokenAmount = IERC20Upgradeable(token).balanceOf(address(this));
+        uint256 tokenAmount = IERC20(token).balanceOf(address(this));
 
         // SB_TAZ: token amount is zero
         require(tokenAmount != 0, "SB_TAZ");
@@ -92,17 +84,15 @@ contract SurplusBeneficiary is
         uint256 tokenAmountToTreasury = FullMath.mulDiv(tokenAmount, _treasuryPercentage, 1e6);
 
         // transfer to treasury first, because FeeDistributor.burn() will transfer all balance from SurplusBeneficiary
-        SafeERC20Upgradeable.safeTransfer(IERC20Upgradeable(token), _treasury, tokenAmountToTreasury);
+        SafeERC20.safeTransfer(IERC20(token), _treasury, tokenAmountToTreasury);
         IFeeDistributor(_feeDistributor).burn(token);
 
-        uint256 balanceAfter = IERC20Upgradeable(token).balanceOf(address(this));
+        uint256 balanceAfter = IERC20(token).balanceOf(address(this));
 
         // SB_BNZ: balance is not zero
         require(balanceAfter == 0, "SB_BNZ");
 
         emit Dispatch(tokenAmountToTreasury, tokenAmount.sub(tokenAmountToTreasury));
-
-        return tokenAmount;
     }
 
     //
